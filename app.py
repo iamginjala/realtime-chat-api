cff143eaea7dea73cfdf04d521f8ea113923d018
Harsha <harshaginjaladec23@gmail.com>
2025-11-24 14:24:10 -0500

    TASK 3: ADD SOCKET.IO

--- app.py
+++ app.py
@@ -7,10 +7,18 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'some secret'
     socketio.init_app(app)
+
+    @app.route('/health')
+    def health():
+        return {'status': 'ok'}
     return app
 
-
-
+@socketio.on('connect')
+def handle_connect():
+    print('client connected ')
+@socketio.on('disconnect')
+def handle_disconnect():
+    print('client disconnected')
 if __name__ == '__main__':
     app = create_app()
     socketio.run(app)
