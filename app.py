106ab3f6d9fb78d8f534c374a5e9eb2a1ed6c5e6
Harsha <harshaginjaladec23@gmail.com>
2025-11-24 14:13:12 -0500

    Task 2: Basic Flask app with /health endpoint

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
+from flask import Flask,render_template
+from flask_socketio import SocketIO
+
+socketio = SocketIO()
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'some secret'
+    socketio.init_app(app)
+    return app
 
 
 
-
-
-
-
-
+if __name__ == '__main__':
+    app = create_app()
+    socketio.run(app)
