d5381248682a33f369ee8e965fba73e3b854781f
Harsha <harshaginjaladec23@gmail.com>
2025-12-04 13:17:04 -0500

    added config files

--- config.py
+++ config.py
@@ -6,3 +6,12 @@ load_dotenv()
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'fallback-secret')
     EXPIRES_IN_HOURS = int(os.getenv('EXPIRES_IN_HOURS', 24))
+
+    DB_USER = os.getenv('DB_USER', 'chatuser')
+    DB_PASSWORD = os.getenv('DB_PASSWORD', 'chatpassword')
+    DB_HOST = os.getenv('DB_HOST', 'localhost')
+    DB_PORT = os.getenv('DB_PORT', '5432')
+    DB_NAME = os.getenv('DB_NAME', 'realtime_chat')
+    
+    SQLALCHEMY_DATABASE_URI = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
